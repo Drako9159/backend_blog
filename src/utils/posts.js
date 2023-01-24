db3e92a5acbfa220c3d798e4698c542093805ce8
drako9159 <drako9159@gmail.com>
2023-01-24 14:45:27 -0600

    edit posts

--- src/utils/posts.js
+++ src/utils/posts.js
@@ -1,7 +1,24 @@
 const fs = require("fs/promises");
 const path = require("path");
+/*
 const MarkdownIt = require("markdown-it");
-const md = new MarkdownIt({ html: true });
+
+
+//const jsx = require("markdown-it-jsx");
+const md = new MarkdownIt({ html: false });
+const parser = new MarkdownIt({
+  html: false, // desactivamos el uso de HTML dentro del markdown
+  breaks: true, // transforma los saltos de línea a un <br />
+  linkify: true, // detecta enlaces y los vuelve enlaces
+  xhtmlOut: true, // devuelve XHTML válido (por ejemplo <br /> en vez de <br>)
+  typographer: true, // reemplaza ciertas palabras para mejorar el texto
+  langPrefix: "language-" // agrega una clase `language-[lang]` a los bloques de código
+});
+*/
+const md = require("markdown-it")();
+const jsx = require("markdown-it-jsx")
+md.use(jsx)
+//md.use(jsx)
 
 async function readerPosts() {
   return await fs
